5b12f560ceb890bd9d164581e7f8b1c907ae631f
Adrianmarinchorro <adrian.marin.chorro@gmail.com>
2023-02-01 20:47:49 +0100

    paso 60 - 12ºpdf

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -3,11 +3,13 @@
 namespace Tests\Browser;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
 class ExampleTest extends DuskTestCase
 {
+
     /**
      * A basic browser test example.
      *
@@ -23,63 +25,4 @@ class ExampleTest extends DuskTestCase
         });
     }
 
-    /**
-     * A basic browser test example.
-     *
-     * @return void
-     */
-    public function testBasicExample2()
-    {
-
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                ->assertSee('Categorías');
-
-        });
-    }
-
-    /**
-     * A basic browser test example.
-     *
-     * @return void
-     */
-    public function testBasicExample3()
-    {
-
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                ->assertSee('Categorías');
-
-        });
-    }
-
-    /**
-     * A basic browser test example.
-     *
-     * @return void
-     */
-    public function testBasicExample4()
-    {
-
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                ->assertSee('Categorías')
-                ->screenshot('4example-test');
-        });
-    }
-
-    /**
-     * A basic browser test example.
-     *
-     * @return void
-     */
-    public function testBasicExample5()
-    {
-
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                ->assertSee('Categorías')
-                ->screenshot('5example-test');
-        });
-    }
 }
