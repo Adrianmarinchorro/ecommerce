652904297ac54318a7a6d10e3da6df9b7c3cc199
Adrianmarinchorro <adrian.marin.chorro@gmail.com>
2023-02-03 20:34:33 +0100

    paso 69 - 14ºpdf

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -5,15 +5,14 @@
     </button>
     <div class="absolute w-full mt-1 hidden" :class="{ 'hidden' : !$wire.open }" @click.away="$wire.open = false">
         <div class="bg-white rounded-lg shadow-lg">
-            <div class="px-4 py-3 space-y-1">
                 @forelse ($products as $product)
-                    <div class="flex">
+                    <a href="{{ route('products.show', $product) }}" class="flex">
                         <img class="w-16 h-12 object-cover" src="{{ Storage::url($product->images->first()->url) }}">
                         <div class="ml-4 text-gray-700">
                             <p class="text-lg font-semibold leading-5">{{$product->name}}</p>
                             <p>Categoria: {{$product->subcategory->category->name}}</p>
                         </div>
-                    </div>
+                    </a>
                 @empty
                     <p class="text-lg leading-5">
                         No existe ningún registro con los parámetros especificados
@@ -22,4 +21,3 @@
             </div>
         </div>
     </div>
-</div>
