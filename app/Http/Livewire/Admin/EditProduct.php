f4f319eefea6e5994cd1b617ff895f7938221acd
Adrianmarinchorro <adrian.marin.chorro@gmail.com>
2023-02-13 16:44:23 +0100

    paso 134 - 22 pdf

--- app/Http/Livewire/Admin/EditProduct.php
+++ app/Http/Livewire/Admin/EditProduct.php
@@ -7,6 +7,7 @@ use App\Models\Category;
 use App\Models\Product;
 use App\Models\Subcategory;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class EditProduct extends Component
@@ -22,6 +23,7 @@ class EditProduct extends Component
         'product.description' => 'required',
         'product.brand_id' => 'required',
         'product.price' => 'required',
+        'product.quantity' => 'numeric',
     ];
 
     public function mount(Product $product)
@@ -37,6 +39,25 @@ class EditProduct extends Component
         $this->brands = Brand::whereHas('categories', function(Builder $query) {
             $query->where('category_id', $this->category_id);
         })->get();
+    }
+
+    public function updatedProductName($value){
+        $this->product->slug = Str::slug($value);
+    }
+
+    public function save()
+    {
+        $this->rules['product.slug'] = 'required|unique:products,slug,' . $this->product->id;
+
+        if ($this->product->subcategory_id) {
+            if (!$this->subcategory->color && !$this->subcategory->size) {
+                $this->rules['product.quantity'] = 'required|numeric';
+            }
+        }
+
+        $this->validate();
+
+        $this->product->save();
     }
 
     public function getSubcategoryProperty()
