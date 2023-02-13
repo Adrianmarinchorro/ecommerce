1be983437a6eedc1c1c315401a09cf085ffdfe7d
Adrianmarinchorro <adrian.marin.chorro@gmail.com>
2023-02-13 21:41:12 +0100

    paso 145 - 24 pdf

--- resources/views/livewire/admin/size-product.blade.php
+++ resources/views/livewire/admin/size-product.blade.php
@@ -37,6 +37,7 @@
                     </div>
 
                 </div>
+                @livewire('admin.color-size', ['size' => $size], key('color-size-' . $size->id))
             </li>
         @endforeach
     </ul>
