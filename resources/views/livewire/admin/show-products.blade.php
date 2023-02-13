fb7a80fb0cd06293a8bede6e367a9233744bcac2
Adrianmarinchorro <adrian.marin.chorro@gmail.com>
2023-02-13 13:15:17 +0100

    paso 123 - 21 pdf

--- resources/views/livewire/admin/show-products.blade.php
+++ resources/views/livewire/admin/show-products.blade.php
@@ -34,19 +34,19 @@
                                 <tr>
                                     <th scope="col"
                                         class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
-                                        Name
+                                        Nombre
                                     </th>
                                     <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                        Title
+                                        Categoría
                                     </th>
                                     <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                        Status
+                                        Estado
                                     </th>
                                     <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                        Role
+                                        Precio
                                     </th>
                                     <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                                        <span class="sr-only">Edit</span>
+                                        <span class="sr-only">Editar</span>
                                     </th>
                                 </tr>
                                 </thead>
@@ -55,29 +55,32 @@
                                     <tr>
                                         <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
                                             <div class="flex items-center">
-                                                <div class="h-10 w-10 flex-shrink-0">
+                                                <div class="h-10 w-10 flex-shrink-0 object-cover">
                                                     <img class="h-10 w-10 rounded-full"
-                                                         src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                                         src="{{ Storage::url($product->images->first()->url) }}"
                                                          alt="">
                                                 </div>
                                                 <div class="ml-4">
-                                                    <div class="font-medium text-gray-900">Lindsay Walton</div>
-                                                    <div class="text-gray-500">lindsay.walton@example.com</div>
+                                                    <div class="font-medium text-gray-900">
+                                                        {{ $product->name }}
+                                                    </div>
                                                 </div>
                                             </div>
                                         </td>
                                         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                            <div class="text-gray-900">Front-end Developer</div>
-                                            <div class="text-gray-500">Optimization</div>
+                                            <div class="text-gray-900">{{ $product->subcategory->category->name }}</div>
+                                            <div class="text-gray-500">{{ $product->subcategory->name }}</div>
                                         </td>
                                         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                                         <span
-                                            class="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">Active</span>
+                                            class="inline-flex rounded-full bg-{{ $product->status == 1 ? 'red' : 'green'
+}}-100 text-{{ $product->status == 1 ? 'red' : 'green' }}-800">{{ $product->status == 1 ? 'Borrador' : 'Publicado' }}</span>
                                         </td>
-                                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Member</td>
-                                        <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                                            <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span
-                                                    class="sr-only">, Lindsay Walton</span></a>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                            {{ $product->price }} &euro;
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                            <a href="{{ route('admin.products.edit', $product) }}" class="text-indigo-600 hover:text-indigo-900">Editar</a>
                                         </td>
                                     </tr>
                                 @endforeach
