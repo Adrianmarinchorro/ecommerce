714c262ad5551737006b419aa32cc7568a99881d
Adrianmarinchorro <adrian.marin.chorro@gmail.com>
2023-02-13 17:34:30 +0100

    paso 137 - 23 pdf

--- resources/views/livewire/admin/color-product.blade.php
+++ resources/views/livewire/admin/color-product.blade.php
@@ -14,15 +14,15 @@
                     </label>
                 @endforeach
             </div>
-            <x-jet-input-error for="color_id" />
+            <x-jet-input-error for="color_id"/>
         </div>
     </div>
     <div>
         <x-jet-label>
             Cantidad
         </x-jet-label>
-        <x-jet-input type="number" wire:model.defer="quantity" placeholder="Ingrese una cantidad" class="w-full" />
-        <x-jet-input-error for="quantity" />
+        <x-jet-input type="number" wire:model.defer="quantity" placeholder="Ingrese una cantidad" class="w-full"/>
+        <x-jet-input-error for="quantity"/>
     </div>
 
     <div class="flex justify-end items-center mt-4">
@@ -32,6 +32,45 @@
         <x-jet-button wire:loading.attr="disabled" wire:target="save" wire:click="save">
             Agregar
         </x-jet-button>
+    </div>
+
+    <div class="bg-white shadow-lg rounded-lg p-6">
+        <table>
+            <thead>
+            <tr>
+                <th class="px-4 py-2 w-1/3">
+                    Color
+                </th>
+                <th class="px-4 py-2 w-1/3">
+                    Cantidad
+                </th>
+                <th class="px-4 py-2 w-1/3">
+
+                </th>
+            </tr>
+            </thead>
+
+            <tbody>
+            @foreach($productColors as $color)
+                <tr>
+                    <td class="capitalize px-4 py-2">
+                        {{ __(ucfirst($colors->find($color->pivot->color_id)->name)) }}
+                    </td>
+                    <td class="px-4 py-2">
+                        {{ $color->pivot->quantity }} unidades
+                    </td>
+                    <td class="px-4 py-2 flex">
+                        <x-jet-secondary-button class="ml-auto mr-2">
+                            Actualizar
+                        </x-jet-secondary-button>
+                        <x-jet-danger-button>
+                            Eliminar
+                        </x-jet-danger-button>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
     </div>
 
 </div>
