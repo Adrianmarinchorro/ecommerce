993d3c388cf635ca61023cc0f3b12aa8afe9b99d
Adrian <adrian.marin.chorro@gmail.com>
2023-02-15 18:45:50 +0100

    paso 166 - 26º pdf

--- app/Http/Livewire/Admin/CreateCategory.php
+++ app/Http/Livewire/Admin/CreateCategory.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire\Admin;
 
 use App\Models\Brand;
 use App\Models\Category;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Livewire\Component;
 use Livewire\WithFileUploads;
@@ -12,7 +13,7 @@ class CreateCategory extends Component
 {
     use WithFileUploads;
 
-    public $brands, $categories, $image;
+    public $brands, $categories, $image, $image2;
     public $category;
     public $editImage;
 
@@ -100,9 +101,12 @@ class CreateCategory extends Component
 
     public function edit(Category $category)
     {
+        $this->image2 = rand();
+
         $this->image = rand();
 
         $this->reset(['editImage']);
+        $this->resetValidation();
 
         $this->category = $category;
 
@@ -127,6 +131,19 @@ class CreateCategory extends Component
         }
 
         $this->validate($rules);
+
+        if ($this->editImage) {
+            Storage::disk('public')->delete($this->editForm['image']);
+            $this->editForm['image'] = $this->editImage->store('categories', 'public');
+        }
+
+        $this->category->update($this->editForm);
+
+        $this->category->brands()->sync($this->editForm['brands']);
+
+        $this->reset(['editForm', 'editImage']);
+
+        $this->getCategories();
     }
 
     public function updatedEditFormName($value)
