925294bf389f4cbfa6f73a0ddf866b45b81a8638
Adrian <adrian.marin.chorro@gmail.com>
2023-02-15 17:45:00 +0100

    paso 164 (/livewire/admin/createCategory y livewire/admin/create-category.blade.php) y arreglo ide bugeado - 26 pdf

--- app/Http/Livewire/Admin/CreateCategory.php
+++ app/Http/Livewire/Admin/CreateCategory.php
@@ -95,6 +95,10 @@ class CreateCategory extends Component
 
     public function edit(Category $category)
     {
+        $this->image = rand();
+
+        $this->reset(['editImage']);
+
         $this->category = $category;
 
         $this->editForm['open'] = true;
