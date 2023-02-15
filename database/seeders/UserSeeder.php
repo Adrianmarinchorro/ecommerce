a286ec4a7f7b8e779430371c9dbc833bc1c609b7
Adrian <adrian.marin.chorro@gmail.com>
2023-02-15 23:50:33 +0100

    paso 181 - 31º pdf

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -14,17 +15,21 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+
+        $role = Role::create(['name' => 'admin']);
+
         User::factory()->create([
             'name' => 'Carlos Abrisqueta',
             'email' => 'carlos@test.com',
-            'password' => bcrypt('123')
-        ]);
+        ])->assignRole('admin');
 
         User::factory()->create([
-            'name' => 'adrian',
+            'name' => 'Adrián Marín',
             'email' => 'adri@test.com',
-            'password' => bcrypt('123')
-        ]);
+            'password' => bcrypt('123'),
+        ])->assignRole('admin');
 
+        User::factory(100)->create();
     }
+
 }
