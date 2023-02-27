48e45f6323a3234b00a03f7e5c45e766eb34a7c8
Adrianmarinchorro <adrian.marin.chorro@gmail.com>
2023-02-27 20:14:26 +0100

    los pedidos mejor hechos

--- resources/views/livewire/admin/products2.blade.php
+++ resources/views/livewire/admin/products2.blade.php
@@ -156,7 +156,7 @@
 
                         <td class="px-6 py-4 whitespace-nowrap">
                             <div class="text-sm text-gray-900">
-                                {{ $product->sold }}
+                                {{ $product->solded }}
                             </div>
                         </td>
 
